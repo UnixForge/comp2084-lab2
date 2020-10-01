008987e9c9832e76cf1343d7e88e4abe3c5e4c57
Lucas Reis <200428201@student.georgianc.on.ca>
2020-09-30 22:39:22 -0400

    Add project files.

---
+++ lab 2/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab_2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: HomeController1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(string category)
+        {
+            ViewBag.category = category;
+            return View();
+        }
+    }
+}
